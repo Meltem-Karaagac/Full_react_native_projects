9912ba6be4d1cf7353098938e862055343395636
meltem <meltemkaraagac19@gmail.com>
2020-10-20 23:32:47 -0400

    yelp_page

---
+++ yelp_page/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { SafeAreaView, Text, View } from "react-native";
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>
+          HELLO
+        </Text>
+      </View>
+    </SafeAreaView>
+  )
+},
+export default App;
