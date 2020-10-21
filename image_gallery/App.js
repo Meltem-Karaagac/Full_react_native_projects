e692021df8aa586959d79d0c0206c8cfbc9b9a53
meltem <meltemkaraagac19@gmail.com>
2020-10-20 23:13:24 -0400

    gallery

---
+++ image_gallery/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { SafeAreaView, View, Text } from "react-native";
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>
+          hello
+        </Text>
+      </View>
+    </SafeAreaView>
+  )
+};
+
+export default App;
