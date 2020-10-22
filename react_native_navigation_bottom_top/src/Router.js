6036c124d769601fdbc5832f8a9973ece0b5e58a
meltem <meltemkaraagac19@gmail.com>
2020-10-22 15:30:19 -0400

    nadd

--- react_native_navigation_bottom_top/src/Router.js
+++ react_native_navigation_bottom_top/src/Router.js
@@ -4,15 +4,16 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-import { Friends, Main, Post } from './pages'
+import { Friends, Timeline, Post } from './pages'
 
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
 
+
 function MainComponent() {
     return (
-        <Stack.Navigator>
-            <Stack.Screen name="MainPage" component={Main} />
+        <Stack.Navigator initialRouteName="MainPage">
+            <Stack.Screen name="MainPage" component={Timeline} />
             <Stack.Screen name="PostPage" component={Post} />
         </Stack.Navigator>
     )
@@ -21,9 +22,17 @@ function MainComponent() {
 function Router() {
     return (
         <NavigationContainer>
-            <Tab.Navigator initialRouteName="PostPage" tabBarOptions={{ activeTintColor: 'purple' }}>
-                <Tab.Screen name="FriendsPage" component={Friends} options={{ title: 'Arkadaşlar' }} />
-                <Tab.Screen name="MainComponentPage" component={MainComponent} options={{ title: 'Ana Sayfa' }} />
+            <Tab.Navigator initialRouteName="MainComponentPage" tabBarOptions={{ activeTintColor: 'purple' }}>
+                <Tab.Screen
+                    name="FriendsPage"
+                    component={Friends}
+                    options={{ title: 'Arkadaşlar' }}
+                />
+                <Tab.Screen
+                    name="MainComponentPage"
+                    component={MainComponent}
+                    options={{ title: 'Ana Sayfa' }}
+                />
             </Tab.Navigator>
         </NavigationContainer>
     );
