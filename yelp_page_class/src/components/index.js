e86340b9eb65b51252e27f0d5d779040d1f8a439
meltem <meltemkaraagac19@gmail.com>
2020-10-25 12:57:19 -0400

    add

---
+++ yelp_page_class/src/components/index.js
@@ -0,0 +1,2 @@
+export * from './CityItem';
+export * from './SearchBar';
--- yelp_page_class/src/pages/CityList.js
+++ yelp_page_class/src/pages/CityList.js
@@ -1,11 +1,39 @@
-import React from 'react';
-import { SafeAreaView, View, Text } from 'react-native';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import { SafeAreaView, View, Text, FlatList } from 'react-native';
+
+import { CityItem, SearchBar } from '../components'
 
 const CityList = (props) => {
+    const [cityList, setCityList] = useState([]);
+
+    // ASYNC-AWAIT 
+    const fetchCityData = async () => {
+        const { data } = await axios.get("https://opentable.herokuapp.com/api/cities");
+        setCityList(data.cities);
+    }
+
+    useEffect(() => {
+        fetchCityData();
+    }, [])
+
+    const renderCities = ({ item }) => <CityItem cityName={item} />
+
+    const renderSeperator = () => <View style={{ borderWidth: 1, borderColor: '#e0e0e0' }} />
+
     return (
         <SafeAreaView>
             <View>
-                <Text>CityList</Text>
+                <SearchBar 
+                    placeholder="Bir şehir arayın..."
+                    onSearch={(value) => console.log(value)}
+                />
+                <FlatList
+                    keyExtractor={(_, index) => index.toString()}
+                    data={cityList}
+                    renderItem={renderCities}
+                    ItemSeparatorComponent={renderSeperator}
+                />
             </View>
         </SafeAreaView>
     )
