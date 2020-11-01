54840acb5edbb9790930fb9556ff4f4cc8912fc8
meltem <meltemkaraagac19@gmail.com>
2020-11-01 13:39:15 -0500

    add

--- Firebase_app/src/LoginPage.js
+++ Firebase_app/src/LoginPage.js
@@ -1,14 +1,49 @@
 import React from 'react';
-import { SafeAreaView, View, Text } from 'react-native';
+import { SafeAreaView, View, Text, TextInput, Button } from 'react-native';
 
 const LoginPage = (props) => {
+    const [email, setEmail] = React.useState("");
+    const [password, setPassword] = React.useState("");
+
+    const signIn = () => {
+
+    }
+
+    const signUp = () => {
+
+    }
+
     return (
         <SafeAreaView>
             <View>
-                <Text>Login</Text>
+                <Text style={{ fontWeight: 'bold', fontSize: 20, textAlign: 'center' }}>Login</Text>
+
+                <View style={{ backgroundColor: '#eceff1', padding: 5, margin: 5, borderRadius: 5 }}>
+                    <TextInput
+                        placeholder="Type your mail.."
+                        onChangeText={text => setEmail(text)}
+                    />
+                </View>
+
+                <View style={{ backgroundColor: '#eceff1', padding: 5, margin: 5, borderRadius: 5 }}>
+                    <TextInput
+                        placeholder="Type your password"
+                        onChangeText={text => setPassword(text)}
+                    />
+                </View>
+
+                <Button
+                    title="Sign Up"
+                    onPress={signUp}
+                />
+                <Button
+                    title="Sign In"
+                    onPress={signIn}
+                />
+
             </View>
         </SafeAreaView>
     );
 }
 
-export default LoginPage;
+export default LoginPage;   
