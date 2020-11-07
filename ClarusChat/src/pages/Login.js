c6fc6190ef56c6ad0280e436b957bd8671490031
meltem <meltemkaraagac19@gmail.com>
2020-11-07 14:29:27 -0500

    add

--- ClarusChat/src/pages/Login.js
+++ ClarusChat/src/pages/Login.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
   SafeAreaView,
-  Dimensions,
   View,
+  Alert,
   Text,
   Image,
   ScrollView,
@@ -11,16 +11,59 @@ import {
 
 import {authStyle} from './styles';
 import {Input, Button} from '../components';
+import {resolveAuthError} from '../functions';
 import auth from '@react-native-firebase/auth';
 
 const Login = (props) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  async function login() {
+    // auth()
+    //   .signInWithEmailAndPassword(email, password)
+    //   .then(() => alert("OK"))
+    //   .catch((err) => Alert.alert("ClarusChat", resolveAuthError(err.code)));
+    try {
+      await auth().signInWithEmailAndPassword(email, password);
+    } catch (error) {
+      Alert.alert('ClarusChat', resolveAuthError(error.code));
+    }
+  }
+
   return (
     <SafeAreaView style={{flex: 1}}>
       <KeyboardAvoidingView style={{flex: 1, backgroundColor: '#cfd8dc'}}>
         <ScrollView contentContainerStyle={{flex: 1}}>
-          {
-            // ...
-          }
+          <View style={authStyle.container}>
+            <Image
+              style={authStyle.logo}
+              source={require('../assets/logo.jpeg')}
+            />
+            <Text style={authStyle.logoText}>ClarusChat</Text>
+          </View>
+          <View style={{flex: 1}}>
+            <Input
+              inputProps={{
+                placeholder: 'Type your email address..',
+                keyboardType: 'email-address',
+              }}
+              onType={(value) => setEmail(value)}
+            />
+            <Input
+              inputProps={{
+                placeholder: 'Type your password..',
+                secureTextEntry: true,
+              }}
+              onType={(value) => setPassword(value)}
+            />
+
+            <Button title="Sign In" onPress={() => login()} />
+            <Button
+              title="Sign Up"
+              noBorder
+              onPress={() => props.navigation.navigate('Sign')}
+            />
+          </View>
         </ScrollView>
       </KeyboardAvoidingView>
     </SafeAreaView>
