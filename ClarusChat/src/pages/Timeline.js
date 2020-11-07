efeb5f2089bb9b3fa72df2873b440331212e3595
meltem <meltemkaraagac19@gmail.com>
2020-11-07 14:36:51 -0500

    add

--- ClarusChat/src/pages/Timeline.js
+++ ClarusChat/src/pages/Timeline.js
@@ -1,16 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {SafeAreaView, View, Text, FlatList} from 'react-native';
 
 import {timelinePage} from './styles';
 import {PostItem, PostInput, Header, TopicSelectModal} from '../components';
 
 const Timeline = () => {
+  const [selectedTopic, setSelectedTopic] = useState(null);
+  const [topicModalFlag, setTopicModalFlag] = useState(false);
+
   return (
     <SafeAreaView style={timelinePage.container}>
       <View style={timelinePage.container}>
-        {
-          // ...
-        }
+        <Header
+          title={selectedTopic}
+          onTopicModalSelect={() => setTopicModalFlag(true)}
+        />
+
+        <FlatList data={[]} renderItem={() => null} />
+
+        <PostInput onSendPost={(value) => console.log(value)} />
+
+        <TopicSelectModal
+          visibility={topicModalFlag}
+          onClose={() => setTopicModalFlag(false)}
+          onTopicSelect={(value) => {
+            setSelectedTopic(value);
+            setTopicModalFlag(false);
+          }}
+        />
       </View>
     </SafeAreaView>
   );
